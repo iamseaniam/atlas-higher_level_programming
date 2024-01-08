947f63e050ef668a1f8cea6d467ec8bcccc886f1
iamseaniam <7222@holbertonstudents.com>
2024-01-07 23:16:10 -0600

    muhaha

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    add_0 = __import__('add_0').add
     a = 1
     b = 2
-    add_0 = __import__('add_0').add
 
     print("{} + {} = {}".format(a, b, add_0(a, b)))
