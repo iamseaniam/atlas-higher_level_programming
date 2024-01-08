19ae49f251348299a1746684ecbc009d25428ad7
iamseaniam <7222@holbertonstudents.com>
2024-01-07 21:27:50 -0600

    aaaa

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def pow(a, b):
+    result = 1
+
+    if b < 0:
+        a, b = 1 / a, -b
+
+    for _ in range(b):
+        result *= a
+
+    return result
+
+if __name__ == "__main__":
+    print(pow(2, 2))
+    print(pow(98, 2))
+    print(pow(98, 0))
+    print(pow(100, -2))
+    print(pow(-4, 5))
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        uppercase_char = chr(ord(char) - 32) if ord('a') <= ord(char) <= ord('z') else char
+        print("{}".format(uppercase_char), end="")
+    print()
+
+if __name__ == "__main__":
+    uppercase("best")
+    uppercase("Best School 98 Battery street")
