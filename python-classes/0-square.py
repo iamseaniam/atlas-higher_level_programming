5a5bc47bd958c53e5246c98ce20a17579da1e603
iamseaniam <7222@holbertonstudents.com>
2024-01-13 14:26:21 -0600

    meh

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,9 +3,10 @@
 This is a module for demonstrating classes in Python.
 """
 
+
 class Square:
-   """
-   This is a class representing a square.
-   It has methods for calculating the area and perimeter.
-   """
-   pass
+    """
+    This is a class representing a square.
+    It has methods for calculating the area and perimeter.
+    """
+    pass
