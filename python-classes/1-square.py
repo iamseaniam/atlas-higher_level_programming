e374cd8c70ec2f8997e80abf2b2d9d551417f34e
iamseaniam <7222@holbertonstudents.com>
2024-01-13 20:12:11 -0600

    errors

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,10 +5,9 @@ This is a module for demonstrating classes in Python.
 
 
 class Square:
-  """
-  This class represents a square. It has a private attribute '__size' which represents the size of the square.
-  The size of the square is set during instantiation and cannot be changed afterwards.
-  """
-  def __init__(self, size):
-      self.__size = size
-      
+    """
+    This class represents a square. It has a private attribute '__size' which represents the size of the square.
+    The size of the square is set during instantiation and cannot be changed afterwards.
+    """
+    def __init__(self, size):
+        self.__size = size
