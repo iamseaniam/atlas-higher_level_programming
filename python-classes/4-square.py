1e58284e2894bcfcbe27e2e5a3946900185d7fe3
iamseaniam <7222@holbertonstudents.com>
2024-01-16 13:57:25 -0600

    gah

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This is a module for demonstrating classes in Python.
+"""
+
+
+class Square:
+  """
+  This class represents a square. It has a private attribute '__size' which represents the size of the square.
+  The size of the square is set during instantiation and cannot be changed afterwards.
+  """
+  def __init__(self, size=0):
+      self.size = size
+
+  @property
+  def size(self):
+      return self.__size
+
+  @size.setter
+  def size(self, value):
+      if not isinstance(value, int):
+          raise TypeError("size must be an integer")
+      elif value < 0:
+          raise ValueError("size must be >= 0")
+      else:
+          self.__size = value
+
+  def area(self):
+      return self.__size ** 2
