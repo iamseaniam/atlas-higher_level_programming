33969a1d14adfe7976f9f90d636c7e0fed5fd3c0
iamseaniam <7222@holbertonstudents.com>
2024-01-16 14:01:01 -0600

    gahsnd

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,18 +5,18 @@ This is a module for demonstrating classes in Python.
 
 
 class Square:
-   """
-   This class represents a square. It has a private attribute '__size' which represents the size of the square.
-   The size of the square is set during instantiation and cannot be changed afterwards.
-   """
-   def __init__(self, size=0):
-       if not isinstance(size, int):
-           raise TypeError("size must be an integer")
-       elif size < 0:
-           raise ValueError("size must be >= 0")
-       else:
-           self.__size = size
+    """
+    This class represents a square. It has a private attribute '__size'.
+    The size of the square is set during instantiation and cannot be cha.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
-   def area(self):
-       return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
    
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,25 +5,25 @@ This is a module for demonstrating classes in Python.
 
 
 class Square:
-  """
-  This class represents a square. It has a private attribute '__size' which represents the size of the square.
-  The size of the square is set during instantiation and cannot be changed afterwards.
-  """
-  def __init__(self, size=0):
-      self.size = size
+    """
+    This class represents a square. It has a private attribute '__size'.
+    The size of the square is set during instantiation and cannot be cha.
+    """
+    def __init__(self, size=0):
+        self.size = size
 
-  @property
-  def size(self):
-      return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-  @size.setter
-  def size(self, value):
-      if not isinstance(value, int):
-          raise TypeError("size must be an integer")
-      elif value < 0:
-          raise ValueError("size must be >= 0")
-      else:
-          self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-  def area(self):
-      return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
