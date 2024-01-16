7f35686e5b0008b08c249c3ef1dddec9f91cf44d
iamseaniam <7222@holbertonstudents.com>
2024-01-16 14:10:12 -0600

    moooo mooo meadoew this stuff is fluff muff

---
+++ python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This is a module for demonstrating classes in Python.
+"""
+
+
+class Square:
+    """
+    This class represents a square. It has a private attribute '__size'.
+    The size of the square is set during instantiation and cannot be cha.
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self._size = value
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or any(i < 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self._position = value
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.position[1]):
+                print()
+            for _ in range(self.size):
+                print(" " * self.position[0] + "#" * self.size)
