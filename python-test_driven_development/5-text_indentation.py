ddaaabbe1ef2ae6ddb47adc0e7116af9b94ba725
iamseaniam <7222@holbertonstudents.com>
2024-01-17 14:13:16 -0600

    errors

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,16 +9,6 @@ def text_indentation(text):
 
     Raises:
         TypeError: If text is not a string.
-
-    Example:
-        >>> text_indentation("Lorem ipsum dolor sit amet. Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? Non autem hoc: igitur ne illud quidem.")
-        Lorem ipsum dolor sit amet.
-        
-        Quonam modo?
-        
-        Utrum igitur tibi litteram videor an totas paginas commovere?
-        
-        Non autem hoc: igitur ne illud quidem.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
@@ -31,7 +21,7 @@ def text_indentation(text):
         current_line += char
         if char in separators:
             lines.append(current_line.strip())
-            lines.append("")  # Add an empty line
+            lines.append("") # Add an empty line
             current_line = ""
 
     if current_line:
