f62d93b9cfa5c3f95252fafa8ddd6af2a2ae88f9
iamseaniam <7222@holbertonstudents.com>
2024-01-23 15:59:01 -0600

    documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 '''this is how you document'''
 
+class BaseGeometry:
+    '''BaseGeometry class definition'''
+
+    def area(self):
+        '''Placeholder for area calculation'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''Validate integer values'''
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
-    '''this is how you document'''
+    '''Rectangle class definition'''
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
