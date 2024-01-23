07484d1859172cae1b95097bc621fa32480c075f
iamseaniam <7222@holbertonstudents.com>
2024-01-23 15:43:41 -0600

    goddddd dammmmnnnnn

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+'''this is how you document'''
 
+
+class BaseGeometry:
+    '''this is how you document'''
+    def area(self):
+        raise Exception("area() is not implemented")
--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+'''this is how you document'''
 
+
+class BaseGeometry:
+    '''this is how you document'''
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
