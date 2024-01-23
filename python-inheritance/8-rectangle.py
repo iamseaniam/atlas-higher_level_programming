5b1abb457c48038007e07cb20089d16677fb43f8
iamseaniam <7222@holbertonstudents.com>
2024-01-23 15:53:29 -0600

    8 z= 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+'''this is how you document'''
 
+
+class Rectangle(BaseGeometry):
+    '''this is how you document'''
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
