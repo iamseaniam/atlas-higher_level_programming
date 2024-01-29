a4f5b874d1755550c624ef21229c04611952667b
iamseaniam <7222@holbertonstudents.com>
2024-01-29 13:01:33 -0600

    getter and setter for parameters

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,25 +1,51 @@
 #!/usr/bin/python3\
-"""Rectangle documentation"""
+"""Rectangle module"""
 
 
 from .base import Base
 
 class Rectangle(Base):
-    """Rectangle documentation"""
+    """Rectangle class inheriting from Base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor for Rectangle class"""
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+
+        def width(self):
+            """Getter for Width"""
+            return self.__width
         
-        def set_width(self, value):
+        def width (self, value):
+            """Setter for Width"""
             self.__width = value
-
-        def set_height(self, value):
+            
+        def height(self):
+            """Getter for height"""
+            return self.__height
+        
+        def height (self, value):
+            """setter for Height"""
             self.__height = value
-
-        def set_x(self, value):
+            
+        def x(self):
+            """getter for X"""
+            return self.x
+        
+        def x(self, value):
+            """Setter for X"""
             self.__x = value
-
-        def set_y(self, value):
-            self.__y = value
+    
+        def y(self):
+            """getter for y"""
+            return self.__y
+        
+        def y(self, value):
+            """Setter for Y"""
+            self.__y = value       
+        
+            
+        
