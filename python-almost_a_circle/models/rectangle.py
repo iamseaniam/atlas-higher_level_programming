4f990b3b4c4cc8b502f9b019a4a404e7ee10945e
iamseaniam <7222@holbertonstudents.com>
2024-01-29 12:31:42 -0600

    documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@
 from .base import Base
 
 class Rectangle(Base):
-
+    """Rectangle documentation"""
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
