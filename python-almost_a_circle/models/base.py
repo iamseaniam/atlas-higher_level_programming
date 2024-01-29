2686a22492b304a22fab3643c28a46e97c8c1046
iamseaniam <7222@holbertonstudents.com>
2024-01-29 11:50:25 -0600

    testing

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,7 +5,7 @@ class Base:
     """This class is thoroughly documented"""
     __nb_objects = 0
     
-    def __init__(self, id=None):
+    def __init__(self, id = None):
         if id is not None:
             self.id = id
         else:
