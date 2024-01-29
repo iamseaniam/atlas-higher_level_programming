a305802f1c99f7eff6f8261acabec0b9f4f24850
iamseaniam <7222@holbertonstudents.com>
2024-01-29 11:35:13 -0600

    syntax

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,7 +5,7 @@ class Base:
     """this stuff is througly documented"""
     __nb_objects = 0
     
-    def __init__(self, id=None):
+    def __init__(self, id = None):
         if id is not None:
             self.id = id
         """
