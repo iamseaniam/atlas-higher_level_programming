5c460b1ef9f17073a80546677b41a59c965158a5
iamseaniam <7222@holbertonstudents.com>
2024-01-29 11:55:45 -0600

    pep8

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Base documentation"""
 
+
 class Base:
     """This class is thoroughly documented"""
     __nb_objects = 0
-    
-    def __init__(self, id = None):
+
+    def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
