8c40bd521232cf93687f7fb99d166fdd69875aff
iamseaniam <7222@holbertonstudents.com>
2024-01-30 12:31:06 -0600

    white space

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,16 +10,16 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Constructor for Square class"""
         super().__init__(size, size, x, y, id)
-        
+
     def __str__(self):
         """Override the __str__ method"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-    
+
     @property
     def size(self):
         """Getter for size"""
         return self.width
-    
+
     @size.setter
     def size(self, value):
         """setter for size"""
