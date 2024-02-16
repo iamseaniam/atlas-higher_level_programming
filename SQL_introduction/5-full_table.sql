e59c1c0b5b9bcc88fcce35e802718d710914cc7a
iamseaniam <7222@holbertonstudents.com>
2024-02-16 14:14:08 -0600

    aaaaaaaaaaaaaaaaaaaaaaa

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- prints description of table
 
-DESCRIBE first_table;
-
-USE hbtn_0c_0;
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
