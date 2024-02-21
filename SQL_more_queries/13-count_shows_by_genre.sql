ffedffa4c2bb81e062e182a7851d0ff8a15cdbab
iamseaniam <7222@holbertonstudents.com>
2024-02-20 20:34:06 -0600

    I DID THEM  BACK WARDS HOW COULD I

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
 -- jdnjen
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM (tv_show_genres RIGHT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id)
-WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC;
+SELECT tv_genres.name AS genre,
+COUNT(*) AS number_of_shows 
+FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+GROUP BY genre ORDER BY number_of_shows DESC;
