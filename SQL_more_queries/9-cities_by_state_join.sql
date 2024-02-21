ad5e060d6b5a3b8065c5ee7eed57431ff54be103
iamseaniam <7222@holbertonstudents.com>
2024-02-20 18:47:34 -0600

    9999999999999999999999

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- helo
-SELECT cities.id, cities.name, (SELECT states.name FROM hbtn_0d_usa.states WHERE states.id = cities.state_id) AS state_name
-FROM hbtn_0d_usa.cities
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
