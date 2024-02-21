f2573a4d14bf95ef4ee6ccc1ce6caa265d9017c8
iamseaniam <7222@holbertonstudents.com>
2024-02-20 19:08:47 -0600

    hmmm

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- hehehehe 
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title,
+       (SELECT tv_show_genres.genre_id FROM @database_name.tv_show_genres WHERE tv_show_genres.show_id = tv_shows.id) AS genre_id
+FROM @database_name.tv_shows
+ORDER BY tv_shows.title ASC, genre_id ASC;
