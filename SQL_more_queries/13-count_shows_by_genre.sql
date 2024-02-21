eebf07d748f49a40135bb231916b5b70cfba9b2f
iamseaniam <7222@holbertonstudents.com>
2024-02-20 20:31:26 -0600

    FOURTEEN AND THIEDTEEN BABY

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,13 +1,4 @@
 -- jdnjen
-SELECT tv_genres.genre AS genre, COUNT(tv_shows.id) AS number_of_shows
-FROM tv_show_genres
-JOIN (
-    SELECT id, title
-    FROM tv_shows
-) AS tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-JOIN (
-    SELECT id, genre
-    FROM genres
-) AS tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.genre
-ORDER BY number_of_shows DESC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM (tv_show_genres RIGHT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id)
+WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC;
--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,5 @@
 -- ehhhhhhhhhh
-SELECT genres.name
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY genres.name ASC;
-
+SELECT tv_genres.name AS genre,
+COUNT(*) AS number_of_shows 
+FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+GROUP BY genre ORDER BY number_of_shows DESC;
