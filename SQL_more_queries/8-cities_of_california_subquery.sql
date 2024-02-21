4e1c9ad4da3b7bc35cd0ed871db2ee5ee7135efb
iamseaniam <7222@holbertonstudents.com>
2024-02-20 18:41:58 -0600

    ssssssssssssssssssssssssssssssssssssssss

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,6 @@
 -- more documentaion
-SELECT id, name
-WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California')
-ORDER BY id ASC;
+SELECT cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+AND states.name = 'California'
+ORDER BY cities.id ASC;
