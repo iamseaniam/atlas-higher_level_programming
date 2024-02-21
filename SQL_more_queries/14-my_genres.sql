2a07252a03fe69daad134f3c1696657800549e13
iamseaniam <7222@holbertonstudents.com>
2024-02-20 20:35:51 -0600

    maybe all on one line, teribal readabilt or how ever you spell it

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,2 @@
 -- ehhhhhhhhhh
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM (tv_show_genres RIGHT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id)
-WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC;
+SELECT name FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id JOIN tv_genres ON genre_id = tv_genres.id WHERE title = "Dexter" ORDER BY name ASC;
