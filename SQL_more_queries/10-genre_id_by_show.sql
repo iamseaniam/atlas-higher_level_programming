7ce902898a98d86e1dda42045ea6daf40fea7f57
iamseaniam <7222@holbertonstudents.com>
2024-02-20 18:50:48 -0600

    meow

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- wello there
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows.tv_shows
-WHERE EXISTS (SELECT  1 FROM hbtn_0d_tvshows.tv_show_genres WHERE tv_show_genres.show_id = tv_shows.id)
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
