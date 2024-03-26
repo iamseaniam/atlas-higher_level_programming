d3082c3997575a1255f25ea4d5b24e9373177010
iamseaniam <7222@holbertonstudents.com>
2024-03-26 17:49:42 -0500

    did not define C_fun

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const C_fun = 'C is fun'
 
 if (process.argv[2] && !isNaN(process.argv[2])) {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log(string);
+    console.log(C_fun);
   }
   process.exit();
 } else {
