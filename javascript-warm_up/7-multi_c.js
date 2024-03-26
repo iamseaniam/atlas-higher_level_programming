47aba7f26e946885d14a6dd2fd6eb4167f2410af
iamseaniam <7222@holbertonstudents.com>
2024-03-26 17:51:58 -0500

    renamed c_fun to string

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const c_fun = 'C is fun'
+const string = 'C is fun'
 
 if (process.argv[2] && !isNaN(process.argv[2])) {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log(c_fun);
+    console.log(string);
   }
   process.exit();
 } else {
