f097efd208f2c723e8bd4ec8c4cb461f91fc1220
iamseaniam <7222@holbertonstudents.com>
2024-03-26 17:47:47 -0500

    file 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const C_fun = 'C is fun'
+
+if (process.argv[2] && !isNaN(process.argv[2])) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log(string);
+  }
+  process.exit();
+} else {
+  console.log('Missing number of occurrences');
+}
