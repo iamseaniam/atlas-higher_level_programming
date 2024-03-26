4469268dbf0b041689ab8b82ae55cfdd7947804e
iamseaniam <7222@holbertonstudents.com>
2024-03-26 17:36:52 -0500

    semi validation for 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let Languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const Languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < Languages.length; i++) {
   console.log(Languages[i]);
