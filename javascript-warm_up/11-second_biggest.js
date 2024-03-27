1433587358ff47683893f9b888de52d6bbd54baa
iamseaniam <7222@holbertonstudents.com>
2024-03-26 22:28:40 -0500

    fixed semi validation

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2);
 if (args.length <= 1) {
-  console.log(0)
+  console.log(0);
 } else {
   args.sort(function (a, b) { return b - a; });
   console.log(args[1]);
