2880c3e2e1d6505ce04bf29c0f4ec459a5023e62
iamseaniam <7222@holbertonstudents.com>
2024-03-26 23:30:57 -0500

    rest of fles

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Rectangle class with constructor and print method
+class Rectangle {
+    constructor (w, h) {
+      if (w >= 1 && h >= 1 && w !== undefined && h !== undefined) {
+      this.width = w;
+      this.height = h;
+      }
+  }
+    
+    print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+      }
+    }
+  }
+  module.exports = Rectangle;
