c9dab36815614ff81f4b75d4005416b451372763
iamseaniam <7222@holbertonstudents.com>
2024-03-26 23:20:49 -0500

    1 through 5 for javascript

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor (w, h) {
+      if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+      }
+    }
+    
+    print () {
+      let i = 0;
+      let j = 0;
+      let string = '';
+      for (; i < this.width; i++) {
+        string += 'X';
+      }
+      for (; j < this.height; j++) {
+        console.log(string);
+      }
+  }
+    
+    rotate () {
+      [this.width, this.height] = [this.height, this.width];
+    }
+    
+    double () {
+      this.height = this.height * 2;
+      this.width = this.width * 2;
+     }
+    }
+    module.exports = Rectangle;
